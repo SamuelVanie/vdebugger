e3fd4c5a3a67d35f118fee2ae73cc43e231c68c9
SamuelVanie <samuelmichaelvanie@gmail.com>
2024-06-30 01:36:00 +0200

    feat: add the command continue to the debugger
    - now you can debug a program by entering his path as an
    argument on the command line

--- src/debugger.rs
+++ src/debugger.rs
@@ -1,10 +1,18 @@
-use linefeed::{Interface, ReadResult};
-use nix::{sys::wait::waitpid, unistd::Pid};
+use std::str::FromStr;
 
+use linefeed::{Interface, ReadResult};
+use nix::{sys::{ptrace::cont, wait::waitpid}, unistd::Pid};
+use strum_macros::EnumString;
+
+#[derive(Debug, PartialEq, EnumString)]
+#[strum(ascii_case_insensitive)]
+enum Command {
+    CONTINUE,
+}
 
 pub struct Debugger {
     m_prog_name: String,
-    m_pid: Pid
+    m_pid: Pid,
 }
 
 impl Debugger {
@@ -12,13 +20,34 @@ impl Debugger {
         Self { m_prog_name, m_pid }
     }
 
+    pub fn continue_execution(&self) {
+        cont(self.m_pid, None).unwrap();
+        let _wait_status = waitpid(self.m_pid, None);
+    }
+
+    pub fn handle_command(&self, command: &String) {
+        let command_line = command.split(" ").collect::<Vec<&str>>();
+        let command = command_line.get(0).unwrap();
+
+        if let Ok(ecommand) = Command::from_str(command) {
+            match ecommand {
+                Command::CONTINUE => self.continue_execution(),
+            }
+        } else {
+            println!("Invalid command entered");
+            unsafe { nix::libc::_exit(0) };
+        }
+    }
+
     pub fn run(&self) {
-        let wait_status = waitpid(self.m_pid, None);
+        let _wait_status = waitpid(self.m_pid, None);
         let reader = Interface::new("vdebugger").unwrap();
         println!("The program name is {}", self.m_prog_name);
         reader.set_prompt("vdebugger> ").unwrap();
 
         while let ReadResult::Input(input) = reader.read_line().unwrap() {
+            self.handle_command(&input);
+            reader.add_history_unique(input.clone());
             println!("got input {:?}", input);
         }
     }
